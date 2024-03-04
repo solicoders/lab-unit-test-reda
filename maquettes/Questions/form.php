33f42dd8f04cb8859f53632de00c4db7ae50fd84
Reda Grain <grain.reda.solicode@gmail.com>
2024-03-04 09:17:34 +0100

    update maquette

--- maquettes/Questions/form.php
+++ maquettes/Questions/form.php
@@ -9,21 +9,31 @@
         </div>
 
         <div class="form-group">
-
+            <label for="inputDescription">Question</label>
+            <div class="d-flex">
+                <input name="projectDescription" class="form-control" value="Quel langage est principalement utilisé pour rendre les pages web interactives et dynamiques ?" id="" placeholder="Entrez la description">
+            </div>
         </div>
 
         <div class="form-group">
             <label for="inputDescription">Reponse</label>
-            <div class="d-flex">
-                <input name="projectDescription" class="form-control" value="Reponse de Question" id="" placeholder="Entrez la description">
-                <input type="radio" name="reponse" class="ml-4" value="Question" id="" placeholder="Entrez la description">
+            <div class="d-flex mb-1">
+                <input name="projectDescription" class="form-control" value="JavaScript" id="" placeholder="Entrez la description">
+                <input type="radio" name="reponse" class="ml-4" checked>
+            </div>
+            <div class="d-flex mb-1">
+                <input name="projectDescription" class="form-control" value="HTML" id="" placeholder="Entrez la description">
+                <input type="radio" name="reponse" class="ml-4">
+            </div>
+            <div class="d-flex mb-1">
+                <input name="projectDescription" class="form-control" value="CSS" id="" placeholder="Entrez la description">
+                <input type="radio" name="reponse" class="ml-4">
             </div>
         </div>
-
+        <div class="d-flex">
+            <a href="./index.php" class="btn btn-default">Annuler</a>
+            <button type="submit" class="btn btn-info ml-2">Ajouter</button>
+        </div>
     </div>
 
-    <div class="card-footer">
-        <a href="./index.php" class="btn btn-default">Annuler</a>
-        <button type="submit" class="btn btn-info">Ajouter</button>
-    </div>
 </form>
