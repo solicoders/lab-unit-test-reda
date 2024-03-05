3c3392610df6c41c21a39ba63db36d185f16e033
Reda Grain <grain.reda.solicode@gmail.com>
2024-03-05 12:49:21 +0100

    update base repository

---
+++ App/app/Repositories/BaseRepository.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Repositories;
+use Illuminate\Database\Eloquent\Model;
+
+abstract class BaseRepository
+{
+
+    protected $model;
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+
+    public function index()
+    {
+        return $this->model->all()->first();
+    }
+}
