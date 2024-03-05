3479ef88d9ff933915d4070fad4c2a9d09619821
Reda Grain <grain.reda.solicode@gmail.com>
2024-03-05 10:51:33 +0100

    restore example test

---
+++ App/tests/Unit/ExampleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Tests;
+use PHPUnit\Framework\TestCase;
+use app\Repository\Tests\TestsRepository;
+
+class GestionTestsTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_showing_tests(): void
+    {
+        $this->assertTrue(true);
+    }
+}
