4c9ee7958fe6fc0ea1c99c3e8ff8264def99a4ec
Reda Grain <grain.reda.solicode@gmail.com>
2024-03-06 10:30:11 +0100

    update question test

--- App/tests/Feature/questions/GestionQuestionsTest.php
+++ App/tests/Feature/questions/GestionQuestionsTest.php
@@ -4,7 +4,6 @@ namespace Tests\Feature;
 
 use App\Models\Questions\Questions;
 use App\Repositories\Questions\QuestionsRepository;
-use App\Repositories\Tests\TestsRepository;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -18,31 +17,55 @@ class GestionQuestionsTest extends TestCase
 
     public function test_data_is_stored(): void
     {
-        $TestsRepository = new QuestionsRepository(new Questions);
+        $QuestionsRepository = new QuestionsRepository(new Questions);
 
         $data = [
             'question' => 'What is Laravel?',
             'reponse' => 'Laravel is a framework',
             'test_id' => 1
         ];
-        $response = $TestsRepository->store($data);
-        $this->assertNotNull($response);
+        $QuestionsRepository->store($data);
+        $this->assertDatabaseHas('questions', ['reponse' => 'Laravel is a framework']);
     }
 
     public function test_data_is_returned(): void
     {
-        $TestsRepository = new QuestionsRepository(new Questions);
+        $QuestionsRepository = new QuestionsRepository(new Questions);
 
-        $response = $TestsRepository->index();
+        $response = $QuestionsRepository->index();
         $this->assertNotNull($response);
     }
 
-    public function test_data_is_greater_than_0(): void
-    {
-        $TestsRepository = new QuestionsRepository(new Questions);
 
-        $response = count($TestsRepository->index());
+    public function test_data_is_updated(): void
+    {
+        $QuestionsRepository = new QuestionsRepository(new Questions);
+        $data = [
+            'question' => 'What is Laravel?',
+            'reponse' => 'Laravel is a php framework',
+            'test_id' => 1
+        ];
+        $response = $QuestionsRepository->update($data, 1);
+        $this->assertNotNull($response);
+    }
+
+    public function test_returned_data_is_greater_than_0(): void
+    {
+        $QuestionsRepository = new QuestionsRepository(new Questions);
+
+        $response = count($QuestionsRepository->paginatedData(2));
 
         $this->assertGreaterThan(0, $response, 'Message: ' . $response);
+    }
+
+
+    public function test_data_is_deleted()
+    {
+        $QuestionsRepository = new QuestionsRepository(new Questions);
+        $data = $QuestionsRepository->find(1);
+        if ($data) {
+            $QuestionsRepository->destroy(1);
+        }
+        $this->assertDatabaseMissing('questions', ['id' => 1]);
     }
 }
